6037f1221027e312af52f91022dfd0dd8317c9fb
Vansh4life <77148767+Vansh4life@users.noreply.github.com>
2021-02-17 21:24:36 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,14 @@
+class Food{
+    constructor(){
+    var foodStock;
+    foodImg = loadImage(Images/Milk.png);
+    }
+    display(){
+        if(addFood.mousePressed()){
+
+        }
+        else{
+            
+        }
+    }
+}
